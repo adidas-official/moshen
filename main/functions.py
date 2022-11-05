82a1f2bbf9397c3215a524922ec7fdf76af6d982
adidas-official <blueman11235@gmail.com>
2022-11-05 15:52:22 +0100

    don't log ip

--- main/functions.py
+++ main/functions.py
@@ -28,7 +28,7 @@ def leak_info(request, client, fields, sheet_name):
     now = datetime.now()
     date_time = now.strftime("%d/%m/%Y, %H:%M:%S")
 
-    sh.update(ip_col+str(row), get_client_ip(request))
+    # sh.update(ip_col+str(row), get_client_ip(request))
     sh.update(date_col+str(row), date_time)
 
 
